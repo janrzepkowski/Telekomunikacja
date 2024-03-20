7b0fe5b3210eaed156c1871c2de4d448893f7a62
Frank <reppepf@gmail.com>
2024-03-20 17:46:04 +0100

    code formatting

--- Zadanie1/main.cpp
+++ Zadanie1/main.cpp
@@ -7,13 +7,19 @@
 
 using namespace std;
 
+#define BEFORE_TRANSMISSION "../dane/wyslane_zakodowane.txt"
+#define AFTER_TRANSMISSION "../dane/odebrane_zakodowane.txt"
+
 int main() {
+
+    cout << "Autorzy: Franek Pietrzycki Jan Rzepkowski\n";
 
     while (true) {
         cout << "Wprowadz 1, 2 lub 3: \n"
                 "1. zapisz wiadomosc do pliku wiadomosc.txt\n"
                 "2. zakoduj wiadomosc z pliku wiadomosc.txt\n"
-                "3. odkoduj wiadomosc z pliku kod.txt w konsoli\n";
+                "3. odkoduj wiadomosc z pliku odebrane_zakodowane.txt w konsoli\n"
+                "4. zakoncz dzialanie programu\n";
         int wybor;
         cin >> wybor;
 
@@ -24,12 +30,10 @@ int main() {
             ofstream plik;
             string wiadomoscZPliku;
             plik.open("../dane/wiadomosc.txt");
-
             plik << message;
             if (plik.is_open()) {
                 plik.close();
             }
-
         } else if (wybor == 2) {
             ifstream plik;
             string wiadomoscZPliku;
@@ -40,8 +44,8 @@ int main() {
             }
             Wiadomosc wiadomosc(wiadomoscZPliku);
             Koder kodownik(wiadomosc);
-            ofstream wyjscie("../dane/wyslane_zakodowane.txt");
-            ofstream odebrane("../dane/odebrane_zakodowane.txt");
+            ofstream wyjscie(BEFORE_TRANSMISSION);
+            ofstream odebrane(AFTER_TRANSMISSION);
             for (const auto& slowo : kodownik.zakoduj()) {
                 for (const auto& bit : slowo) {
                     wyjscie << bit;
@@ -50,10 +54,11 @@ int main() {
             }
             wyjscie.close();
             odebrane.close();
-            cout << "Wiadomosc: " + wiadomosc.pobierzWiadomosc();
-            cout << "\nZakodowana wiadomosc: " + kodownik.pobierzZakodowanaWiadomosc() + "\nZapisano do pliku kod.txt";
+            cout << "Wiadomosc: " + wiadomosc.pobierzWiadomosc() << "\n";
+            cout << "Zakodowana wiadomosc: " + kodownik.pobierzZakodowanaWiadomosc() << "\n";
+            cout << "Zapisano do pliku odebrane_zakodowane.txt\n";
         } else if (wybor == 3) {
-            ifstream wejscie("../dane/odebrane_zakodowane.txt");
+            ifstream wejscie(AFTER_TRANSMISSION);
             string slowo;
             vector<vector<bool>> kodZPliku;
             while (!wejscie.eof()) {
@@ -73,19 +78,16 @@ int main() {
             wejscie.close();
 
             Odbiornik odbiornik(kodZPliku);
-            cout << "\nZakodowana wiadomosc otrzymana: " + odbiornik.pobierzKod();
-            cout << "\nPoprawnosc slow kodowych: ";
-            cout << boolalpha;
-            cout << odbiornik.sprawdzPoprawnosc();
+            cout << "Zakodowana wiadomosc otrzymana: " + odbiornik.pobierzKod() << "\n";
+            cout << "Poprawnosc slow kodowych: " << boolalpha;
+            cout << odbiornik.sprawdzPoprawnosc() << "\n";
             if (!odbiornik.sprawdzPoprawnosc()) {
                 odbiornik.naprawBledy();
-                cout << "\nWiadomosc poprawiona: " << odbiornik.pobierzKod();
+                cout << "Wiadomosc poprawiona: " << odbiornik.pobierzKod() << "\n";
             }
             Dekoder dekoder(odbiornik.pobierzZakodowanaWiadomosc());
-            cout << "\nOdkodowana wiadomosc: " + dekoder.odkoduj() << "\n";
+            cout << "Odkodowana wiadomosc: " + dekoder.odkoduj() << "\n";
         } else if (wybor == 4) {
-            // TODO
-        } else {
             break;
         }
     }
